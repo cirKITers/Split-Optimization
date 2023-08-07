baff233ef6c91fb8f364f3e631a19054559e336d
ulivc <ulivc@student.kit.edu>
2023-08-07 10:15:38 +0200

    remove use of import*

--- src/split_optimizer/pipelines/data_science/__init__.py
+++ src/split_optimizer/pipelines/data_science/__init__.py
@@ -3,7 +3,7 @@ This is a boilerplate pipeline 'mnist_processing'
 generated using Kedro 0.18.1
 """
 
-from .pipeline import *
+from .pipeline import create_training_pipeline
 
 __all__ = ["create_training_pipeline"]
 
