7eb80d622ceea95c836d6bb5af9a954a670ae8ed
lc3267 <melvin.strobl@kit.edu>
2023-09-29 20:28:01 +0200

    black
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/split_optimizer/pipelines/data_science/hyperparam_optimizer.py
+++ src/split_optimizer/pipelines/data_science/hyperparam_optimizer.py
@@ -9,16 +9,35 @@ from joblib import parallel_backend
 
 class Hyperparam_Optimizer:
     def __init__(
-        self, name: str, seed: int, path:str, n_trials: int, timeout: int, n_jobs: int=1, selective_optimization:bool=False, toggle_classical_quant:bool=False, resume_study:bool=False, pool_process=True, pruner_startup_trials=10, pruner_warmup_steps=5, pruner_interval_steps=1, pruner_min_trials=20
+        self,
+        name: str,
+        seed: int,
+        path: str,
+        n_trials: int,
+        timeout: int,
+        n_jobs: int = 1,
+        selective_optimization: bool = False,
+        toggle_classical_quant: bool = False,
+        resume_study: bool = False,
+        pool_process=True,
+        pruner_startup_trials=10,
+        pruner_warmup_steps=5,
+        pruner_interval_steps=1,
+        pruner_min_trials=20,
     ):
         # storage = self.initialize_storage(host, port, path, password)
 
-        pruner=o.pruners.MedianPruner(
-            n_startup_trials=pruner_startup_trials, n_warmup_steps=pruner_warmup_steps, interval_steps=pruner_interval_steps, n_min_trials=pruner_min_trials
+        pruner = o.pruners.MedianPruner(
+            n_startup_trials=pruner_startup_trials,
+            n_warmup_steps=pruner_warmup_steps,
+            interval_steps=pruner_interval_steps,
+            n_min_trials=pruner_min_trials,
         )
         # pruner = o.pruners.PercentilePruner(10.0, n_warmup_steps=2, n_startup_trials=20)
 
-        sampler = o.samplers.TPESampler(seed=seed, multivariate=True, constant_liar=True)
+        sampler = o.samplers.TPESampler(
+            seed=seed, multivariate=True, constant_liar=True
+        )
 
         self.n_trials = n_trials
         self.timeout = timeout
@@ -30,19 +49,21 @@ class Hyperparam_Optimizer:
         self.n_jobs = n_jobs
 
         n_studies = self.n_jobs if self.pool_process else 1
-        
+
         for n_it in range(n_studies):
             resume_study = resume_study or (n_studies > 1 and n_it != 0)
 
-            self.studies.append(o.create_study(
-                pruner=pruner,
-                sampler=sampler,
-                # directions=["maximize", "minimize", "maximize"],
-                direction="maximize",
-                load_if_exists=resume_study,
-                study_name=name,
-                storage=f"sqlite:///{path}",
-            ))
+            self.studies.append(
+                o.create_study(
+                    pruner=pruner,
+                    sampler=sampler,
+                    # directions=["maximize", "minimize", "maximize"],
+                    direction="maximize",
+                    load_if_exists=resume_study,
+                    study_name=name,
+                    storage=f"sqlite:///{path}",
+                )
+            )
 
             # set_objective_names(self.studies[-1], ["Accuracy", "Loss", "Perfect LCAG"])
             set_objective_names(self.studies[-1], ["Accuracy"])
@@ -80,7 +101,9 @@ class Hyperparam_Optimizer:
     def log_study(self):
         for study in self.studies:
             for trial in study.best_trials:
-                mlflow.log_params({f"trial_{trial.number}_{k}":v for k, v in trial.params.items()})
+                mlflow.log_params(
+                    {f"trial_{trial.number}_{k}": v for k, v in trial.params.items()}
+                )
 
                 mlflow.log_metric(f"trial_{trial.number}_accuracy", trial.values[0])
                 mlflow.log_metric(f"trial_{trial.number}_loss", trial.values[1])
@@ -166,13 +189,19 @@ class Hyperparam_Optimizer:
             with ProcessPoolExecutor(max_workers=len(self.studies)) as pool:
                 futures = []
                 for study in self.studies:
-                    futures.append(pool.submit(study.optimize, self.run_trial, n_trials=self.n_trials))
+                    futures.append(
+                        pool.submit(
+                            study.optimize, self.run_trial, n_trials=self.n_trials
+                        )
+                    )
 
                 wait(futures)
         else:
-            with parallel_backend('multiprocessing'):
-                self.studies[0].optimize(self.run_trial, n_trials=self.n_trials, n_jobs=self.n_jobs)
-            
+            with parallel_backend("multiprocessing"):
+                self.studies[0].optimize(
+                    self.run_trial, n_trials=self.n_trials, n_jobs=self.n_jobs
+                )
+
     def run_trial(self, trial):
         updated_variable_model_parameters = self.update_variable_parameters(
             trial, self.variable_model_parameters
@@ -180,7 +209,7 @@ class Hyperparam_Optimizer:
         model_parameters = (
             self.fixed_model_parameters | updated_variable_model_parameters
         )
-        model = self.create_model(**model_parameters)["model"]
+        model = self.create_model(**model_parameters)
 
         # gather all parameters and create instructor
         updated_variable_instructor_parameters = self.update_variable_parameters(
@@ -194,9 +223,9 @@ class Hyperparam_Optimizer:
         ] = model  # update this single parameter using the returned model
         instructor_parameters["report_callback"] = trial.report
         instructor_parameters["early_stop_callback"] = self.early_stop_callback
-        instructor = self.create_instructor(**instructor_parameters)["instructor"]
+        instructor = self.create_instructor(**instructor_parameters)
 
-        metrics = self.objective(instructor, trial)["metrics"]
+        metrics = self.objective(instructor=instructor, trial=trial)
 
         return metrics["accuracy"]
         # return [metrics["accuracy"], metrics["loss"], metrics["perfect_lcag"]]
