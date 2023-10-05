c77ce59cf839f38543f545eb271bff77598c780b
lc3267 <melvin.strobl@kit.edu>
2023-10-05 17:03:33 +0200

    pre and post layers
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/split_optimizer/pipelines/data_science/hybrid_model.py
+++ src/split_optimizer/pipelines/data_science/hybrid_model.py
@@ -45,9 +45,9 @@ class QLayers:
         return [qml.expval(qml.PauliZ(i)) for i in range(self.number_classes)]
 
 
-class CLayers(nn.Module):
+class PreCLayers(nn.Module):
     def __init__(self, n_qubits):
-        super(CLayers, self).__init__()
+        super(PreCLayers, self).__init__()
 
         # Bx1x28x28 -> Bx3x14x14
         # self.conv_layer_1 = nn.Sequential(
@@ -78,25 +78,68 @@ class CLayers(nn.Module):
         # x = self.out(x)
         return x
 
+class PostCLayers(nn.Module):
+    def __init__(self, n_qubits, n_classes):
+        super(PostCLayers, self).__init__()
+
+        # Bx1x28x28 -> Bx3x14x14
+        # self.conv_layer_1 = nn.Sequential(
+        #     nn.Conv2d(
+        #         in_channels=1,
+        #         out_channels=3,
+        #         kernel_size=3,
+        #         stride=1,
+        #         padding=1,
+        #     ),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=2),
+        # )
+        self.fc_layer = nn.Sequential(
+            nn.Linear(n_qubits, n_classes),
+            nn.ReLU(),
+        )
+
+        # self.out = nn.Linear(3*14*14, n_qubits)
+
+    def forward(self, x):
+        x = self.fc_layer(x)
+        # x = self.conv_layer_2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        # x = self.flatten(x)
+        # x = self.out(x)
+        return x
 
 class Model(nn.Module):
     def __init__(self, n_qubits, classes, n_layers, data_reupload):
         super(Model, self).__init__()
         self.n_qubits = n_qubits
         self.number_classes = len(classes)
-        self.clayer = CLayers(self.n_qubits)
+        self.pre_clayer = PreCLayers(self.n_qubits)
+        self.post_clayer = PostCLayers(self.n_qubits, self.number_classes)
         dev = qml.device("default.qubit", wires=self.n_qubits)
+        # self.vqc = QLayers(
+        #     self.n_qubits, n_layers, self.number_classes, data_reupload=data_reupload
+        # )
         self.vqc = QLayers(
-            self.n_qubits, n_layers, self.number_classes, data_reupload=data_reupload
+            self.n_qubits, n_layers, self.n_qubits, data_reupload=data_reupload
         )
         self.qnode = qml.QNode(self.vqc.quantum_circuit, dev, interface="torch")
         self.qlayer = qml.qnn.TorchLayer(self.qnode, self.vqc.weight_shape)
 
         self.sm = nn.Softmax(dim=1)  # dim=1 because x will have shape bs x n_classes
 
+    def q_pre_proc(self, x):
+        return (x + 1) * torch.pi / 2
+
     def forward(self, x):
-        x = (self.clayer(x) + 1) * torch.pi / 2
-        x = self.sm(self.qlayer(x))
-        # x = self.clayer(x)
+        # x = (self.clayer(x) + 1) * torch.pi / 2
+        # x = self.sm(self.qlayer(x))
+
+        x = self.pre_clayer(x)
+        # x = self.q_pre_proc(x)
+        x = self.qlayer(x)
+        x = self.post_clayer(x)
+
+        # x = self.sm(x)
 
         return x
