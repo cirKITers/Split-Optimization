a940843795713b84db618576148e2e55743e956f
lc3267 <melvin.strobl@kit.edu>
2023-10-06 14:37:06 +0200

    implemented reset parameter methods for all layers
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/split_optimizer/pipelines/data_science/hybrid_model.py
+++ src/split_optimizer/pipelines/data_science/hybrid_model.py
@@ -10,7 +10,7 @@ class TorchLayer(qml.qnn.TorchLayer):
     def reset_parameters(self):
         pass
 
-class QLayers:
+class QModule:
     def __init__(self, n_qubits, n_layers, number_classes, data_reupload):
         self.number_classes = number_classes
         if not self.number_classes <= n_qubits:
@@ -27,6 +27,8 @@ class QLayers:
 
         self.argnum = range(self.n_qubits, self.n_qubits + self.n_qubits * n_layers)
         self.weight_shape = {"weights": [n_layers, n_qubits, self.vqc(None)]}
+
+    
 
     def quantum_circuit(self, weights, inputs=None):
         if inputs is None:
@@ -48,68 +50,39 @@ class QLayers:
         return [qml.expval(qml.PauliZ(i)) for i in range(self.number_classes)]
 
 
-class PreCLayers(nn.Module):
+class PreClassicalModule(nn.Module):
     def __init__(self, n_qubits):
-        super(PreCLayers, self).__init__()
-
-        # Bx1x28x28 -> Bx3x14x14
-        # self.conv_layer_1 = nn.Sequential(
-        #     nn.Conv2d(
-        #         in_channels=1,
-        #         out_channels=3,
-        #         kernel_size=3,
-        #         stride=1,
-        #         padding=1,
-        #     ),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(kernel_size=2),
-        # )
-        self.fc_layer = nn.Sequential(
-            nn.Linear(1 * 28 * 28, n_qubits),
-            nn.Tanh(),
-        )
+        super(PreClassicalModule, self).__init__()
 
         self.flatten = nn.Flatten()
-        # self.out = nn.Linear(3*14*14, n_qubits)
+        self.l1 = nn.Linear(1 * 28 * 28, n_qubits)
+        self.a1 = nn.Tanh()
+
+    def reset_parameters(self):
+        nn.init.xavier_normal_(self.l1.weight)
+        nn.init.zeros_(self.l1.bias)
 
     def forward(self, x):
         x = self.flatten(x)
-        x = self.fc_layer(x)
-        # x = self.conv_layer_2(x)
-        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
-        # x = self.flatten(x)
-        # x = self.out(x)
+        x = self.l1(x)
+        x = self.a1(x)
         return x
 
-class PostCLayers(nn.Module):
+class PostClassicalModule(nn.Module):
     def __init__(self, n_qubits, n_classes):
-        super(PostCLayers, self).__init__()
+        super(PostClassicalModule, self).__init__()
 
-        # Bx1x28x28 -> Bx3x14x14
-        # self.conv_layer_1 = nn.Sequential(
-        #     nn.Conv2d(
-        #         in_channels=1,
-        #         out_channels=3,
-        #         kernel_size=3,
-        #         stride=1,
-        #         padding=1,
-        #     ),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(kernel_size=2),
-        # )
-        self.fc_layer = nn.Sequential(
-            nn.Linear(n_qubits, n_classes),
-            nn.ReLU(),
-        )
+        self.l1 = nn.Linear(n_qubits, n_classes)
+        self.a1 = nn.ReLU()
 
-        # self.out = nn.Linear(3*14*14, n_qubits)
+
+    def reset_parameters(self):
+        nn.init.xavier_normal_(self.l1.weight)
+        nn.init.zeros_(self.l1.bias)
 
     def forward(self, x):
-        x = self.fc_layer(x)
-        # x = self.conv_layer_2(x)
-        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
-        # x = self.flatten(x)
-        # x = self.out(x)
+        x = self.l1(x)
+        x = self.a1(x)
         return x
 
 class Model(nn.Module):
@@ -117,19 +90,27 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.n_qubits = n_qubits
         self.number_classes = len(classes)
-        self.pre_clayer = PreCLayers(self.n_qubits)
-        self.post_clayer = PostCLayers(self.n_qubits, self.number_classes)
+        self.pre_clayer = PreClassicalModule(self.n_qubits)
+        self.post_clayer = PostClassicalModule(self.n_qubits, self.number_classes)
         dev = qml.device("default.qubit", wires=self.n_qubits)
         # self.vqc = QLayers(
         #     self.n_qubits, n_layers, self.number_classes, data_reupload=data_reupload
         # )
-        self.vqc = QLayers(
+        self.vqc = QModule(
             self.n_qubits, n_layers, self.n_qubits, data_reupload=data_reupload
         )
         self.qnode = qml.QNode(self.vqc.quantum_circuit, dev, interface="torch")
-        self.qlayer = qml.qnn.TorchLayer(self.qnode, self.vqc.weight_shape)
+        self.qlayer = TorchLayer(self.qnode, self.vqc.weight_shape)
 
         self.sm = nn.Softmax(dim=1)  # dim=1 because x will have shape bs x n_classes
+
+    def reset_parameters(self):
+        """
+        This method is intended to be called from the instructor after torch seeds are set
+        """
+        for layer in self.children():
+            if hasattr(layer, "reset_parameters"):
+                layer.reset_parameters()
 
     def q_pre_proc(self, x):
         return (x + 1) * torch.pi / 2
@@ -140,7 +121,7 @@ class Model(nn.Module):
 
         x = self.pre_clayer(x)
         # x = self.q_pre_proc(x)
-        x = self.qlayer(x)
+        # x = self.qlayer(x)
         x = self.post_clayer(x)
 
         # x = self.sm(x)
