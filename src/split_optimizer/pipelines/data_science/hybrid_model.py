55415c5cd3689798fb2bbfe6ecc738597b29322a
lc3267 <melvin.strobl@kit.edu>
2023-10-09 16:58:32 +0200

    get params for c and q
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/split_optimizer/pipelines/data_science/hybrid_model.py
+++ src/split_optimizer/pipelines/data_science/hybrid_model.py
@@ -6,10 +6,12 @@ import torch.nn.functional as F
 
 from .ansaetze import ansaetze
 
+
 class TorchLayer(qml.qnn.TorchLayer):
     def reset_parameters(self):
         # nn.init.uniform_(self.qnode_weights['weights'], b=2 * torch.pi)
-        nn.init.zeros_(self.qnode_weights['weights'])
+        nn.init.zeros_(self.qnode_weights["weights"])
+
 
 class QModule:
     def __init__(self, n_in, n_layers, n_out, data_reupload, disable_learning):
@@ -33,8 +35,6 @@ class QModule:
         else:
             self.argnum = range(self.n_qubits, self.n_qubits + self.n_qubits * n_layers)
             self.weight_shape = {"weights": [n_layers, n_in, self.vqc(None)]}
-
-    
 
     def quantum_circuit(self, weights, inputs=None):
         if inputs is None:
@@ -77,13 +77,13 @@ class PreClassicalModule(nn.Module):
         x = self.a1(x)
         return x
 
+
 class PostClassicalModule(nn.Module):
     def __init__(self, n_qubits, n_classes):
         super(PostClassicalModule, self).__init__()
 
         self.l1 = nn.Linear(n_qubits, n_classes)
         self.a1 = nn.ReLU()
-
 
     def reset_parameters(self):
         nn.init.xavier_normal_(self.l1.weight)
@@ -94,27 +94,40 @@ class PostClassicalModule(nn.Module):
         x = self.a1(x)
         return x
 
+
 class Model(nn.Module):
     def __init__(self, n_qubits, classes, n_layers, data_reupload, quant_status):
         super(Model, self).__init__()
         self.n_qubits = n_qubits
-        self.quant_status=quant_status
+        self.quant_status = quant_status
         self.number_classes = len(classes)
         self.pre_clayer = PreClassicalModule(self.n_qubits)
         self.post_clayer = PostClassicalModule(self.n_qubits, self.number_classes)
 
-        if self.quant_status==0: # passthrough
+        if self.quant_status == 0:  # passthrough
             self.qlayer = nn.Identity()
         else:
             dev = qml.device("default.qubit", wires=self.n_qubits)
             self.vqc = QModule(
-                self.n_qubits, n_layers, self.n_qubits, data_reupload=data_reupload, disable_learning=(self.quant_status==1) # either iec only (1) or iec + pqc (2)
+                self.n_qubits,
+                n_layers,
+                self.n_qubits,
+                data_reupload=data_reupload,
+                disable_learning=(
+                    self.quant_status == 1
+                ),  # either iec only (1) or iec + pqc (2)
             )
             self.qnode = qml.QNode(self.vqc.quantum_circuit, dev, interface="torch")
 
             self.qlayer = TorchLayer(self.qnode, self.vqc.weight_shape)
 
         self.sm = nn.Softmax(dim=1)  # dim=1 because x will have shape bs x n_classes
+
+    def get_clayers(self):
+        return [self.pre_clayer, self.post_clayer]
+
+    def get_qlayers(self):
+        return [self.qlayer]
 
     def reset_parameters(self):
         """
