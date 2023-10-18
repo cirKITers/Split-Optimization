0aa996cf9e03a628ca10219571afe36bd06ac6f1
ulivc <ulivc@student.kit.edu>
2023-10-18 11:32:18 +0200

    optimizer tests and cleanup

--- src/tests/pipelines/data_processing/test_pipeline.py
+++ src/tests/pipelines/data_processing/test_pipeline.py
@@ -15,17 +15,15 @@ def test_data_preparation():
     train_dataloader = output["train_dataloader"]
     test_dataloader = output["test_dataloader"]
 
-    test_dataset = test_dataloader.dataset
-    train_dataset = train_dataloader.dataset
+    
 
-    training_size = train_dataset.data.shape[0]
-    test_size = test_dataset.data.shape[0]
-
-    batch_size_test = test_dataloader.batch_size
     batch_size_train = train_dataloader.batch_size
 
-    train_data = train_dataloader.dataset.data
-    test_data = test_dataloader.dataset.data
+    train_dataset = train_dataloader.dataset.data
+    test_dataset = test_dataloader.dataset.data
+
+    training_size = train_dataset.shape[0]
+    test_size = test_dataset.shape[0]
 
     train_data, _ = next(iter(train_dataloader))
     train_data_size = train_data.size()
@@ -33,9 +31,11 @@ def test_data_preparation():
     test_data, _ = next(iter(test_dataloader))
     test_data_size = test_data.size()
 
+    # test normalization
     assert torch.max(train_data) <= 1, "train_data is not normalized"
     assert torch.max(test_data) <= 1, "test_data is not normalized"
    
+    # test shape
     assert np.all(
         np.array(test_data_size) == np.array([test_size, 1, 28, 28])
     ), f"test_data should have the shape[1, 1, 28, 28] but has the shape {np.array(test_data_size)}"
@@ -43,9 +43,28 @@ def test_data_preparation():
         np.array(train_data_size) == np.array([parameters["batch_size"], 1, 28, 28])
     ), f"train_data should have the shape[{parameters['batch_size']}, 1, 28, 28] but has the shape {np.array(train_data_size)}"
     assert batch_size_train == parameters["batch_size"]
+    
+    #test size of the datasets
     assert (
         training_size == parameters["TRAINING_SIZE"]
     ), f"training_size is {training_size} but should be {parameters['TRAINING_SIZE']}"
     assert (
         test_size == parameters["TEST_SIZE"]
     ), f"test_size is {test_size} but should be {parameters['TEST_SIZE']}"
+
+
+    #check reproducability 
+    session.close()
+    bootstrap_project(Path.cwd())
+    with KedroSession.create() as session:
+        output = session.run(pipeline_name="preprocessing")
+
+
+    second_train_dataloader = output["train_dataloader"]
+    second_test_dataloader = output["test_dataloader"]
+    second_train_data = second_train_dataloader.dataset.data
+    second_test_data = second_test_dataloader.dataset.data
+
+
+    assert torch.all(torch.eq(train_dataset, second_train_data))
+    assert torch.all(torch.eq(test_dataset, second_test_data))
--- src/tests/pipelines/data_science/test_pipeline.py
+++ src/tests/pipelines/data_science/test_pipeline.py
@@ -5,7 +5,23 @@ from kedro.framework.startup import bootstrap_project
 import json
 
 
-def test_data_shape():
+def test_optimizer():
+    # iterate all optimizer, run a training 
+    bootstrap_project(Path.cwd())
+    
+    for i in ["SGD", "Adam"]:
+        for p in ["Adam", "SPSA", "SGD", "NGD", "QNG"]:
+            session = KedroSession.create()
+            parameters = session.load_context().config_loader["parameters"]["data_science"]
+            optimizer = parameters["optimizer"]
+            parameters["epochs"] = 2
+            optimizer["split"]["classical"]["name"] = i
+            optimizer["split"]["quantum"]["name"] = p
+            output = session.run(pipeline_name="debug_pipeline")
+            session.close()
+
+
+def test_training():
 
     bootstrap_project(Path.cwd())
     with KedroSession.create() as session:
@@ -16,12 +32,41 @@ def test_data_shape():
     filepath = metrics_fig["filepath"]
 
     with open(filepath, "r") as file:
-        loss_data = json.load(file)
+        metrics = json.load(file)
 
-    train_loss = loss_data["data"][0]["y"]
+    train_loss = metrics["data"][0]["y"]
+    # start = train_loss[0]
+    # for i, p in enumerate(train_loss):
+    #    assert p < start / (0.5 * (i + 1)), "loss does not decrease efficiently"
 
-    start = train_loss[0]
-    for i, p in enumerate(train_loss):
-        assert p < start / (0.5 * (i + 1)), "loss does not decrease efficiently"
+    parameters = session.load_context().config_loader["parameters"]["data_processing"]
 
-    assert 1 == 1
+    train_accuracy = metrics["data"][1]["y"]
+    val_accuracy = metrics["data"][3]["y"]
+
+    coincidence_accuracy = 1 / len(parameters["classes"])
+
+    # check if accuracy is better than the minimum coincidence case
+    assert (
+        train_accuracy[-1] > coincidence_accuracy
+    ), f"train accuracy should be higher than {coincidence_accuracy}"
+    assert (
+        val_accuracy[-1] > coincidence_accuracy
+    ), f"validation accuracy should be higher than {coincidence_accuracy} "
+
+    # check reproducability
+    session.close()
+    bootstrap_project(Path.cwd())
+    with KedroSession.create() as session:
+        output = session.run(pipeline_name="__default__")
+
+    data_catalog = session.load_context().config_loader["catalog"]
+    metrics_fig = data_catalog["data_science.metrics_fig"]["data_set"]
+    filepath = metrics_fig["filepath"]
+
+    with open(filepath, "r") as file:
+        metrics = json.load(file)
+
+    second_train_loss = metrics["data"][0]["y"]
+
+    assert np.array_equal(second_train_loss, train_loss), "training is not consistent"
